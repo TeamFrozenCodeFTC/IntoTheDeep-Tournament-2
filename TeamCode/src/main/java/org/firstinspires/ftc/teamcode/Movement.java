0ed818ab2bbf5c626239d8c4c387b7b6c8d7c539
PythonDominator <jacobophoven777@gmail.com>
2024-09-21 10:38:21 -0500

    boost and slow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement.java
@@ -19,6 +19,8 @@ public class Movement extends LinearOpMode {
     DcMotor backRightMotor;
     DcMotor frontRightMotor;
 
+    DcMotor intakeMotor;
+
     double frontLeftPower = 0;
     double backLeftPower = 0;
     double backRightPower = 0;
@@ -26,12 +28,9 @@ public class Movement extends LinearOpMode {
 
     ArrayList<Double[][]> controls = new ArrayList<>();
 
-    double rightStickXPressTime = 0;
-    double leftStickYPressTime = 0;
-    double rightTriggerPressTime = 0;
-    double leftTriggerPressTime = 0;
-
-    double secondsToFullSpeed = 3;
+    double slowerFactor = 3;
+    double factor = 0.75;
+    double boostFactor = 3;
 
     @Override
     public void runOpMode() {
@@ -43,27 +42,29 @@ public class Movement extends LinearOpMode {
         backLeftMotor = hardwareMap.get(DcMotor.class, "backLeft");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
         backRightMotor = hardwareMap.get(DcMotor.class, "backRight");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
 
         waitForStart();
 
         while (opModeIsActive()) {
-            calculateButtonHoldTimes();
-
             horizontalSlideControl();
             verticalControl();
             pivotControl();
-            boostControl();
+            intakeControl();
 
             frontLeftPower = 0;
             backLeftPower = 0;
             backRightPower = 0;
             frontRightPower = 0;
             for (Double[][] control : controls) {
-                frontLeftPower += control[0][0];
+                frontLeftPower -= control[0][0];
                 backLeftPower += control[1][0];
                 backRightPower += control[1][1];
                 frontRightPower += control[0][1];
             }
+
+            boostControl();
+            slowControl();
 
             // Set Motor Wheel Power
             frontLeftMotor.setPower(frontLeftPower);
@@ -71,78 +72,51 @@ public class Movement extends LinearOpMode {
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
 
-            //telemetry.addData("Status");
-            telemetry.update();
+            controls = new ArrayList<>();
         }
     }
 
-    void calculateButtonHoldTimes() {
-        Map<String, Double[]> pressTimes = new HashMap<>();
-        pressTimes.put("rightStickXPressTime", new Double[] {(double)gamepad1.right_stick_x, rightStickXPressTime});
-        pressTimes.put("leftStickYPressTime", new Double[] {(double)gamepad1.left_stick_y, leftStickYPressTime});
-        pressTimes.put("leftTriggerPressTime", new Double[] {(double)gamepad1.left_trigger, leftTriggerPressTime});
-        pressTimes.put("rightTriggerPressTime", new Double[] {(double)gamepad1.right_trigger, rightTriggerPressTime});
-
-        for (Map.Entry<String, Double[]> entry : pressTimes.entrySet()) {
-            String key = entry.getKey();
-
-            Double[] value = entry.getValue();
-            Double button = value[0];
-            Double timePressed = value[1];
-
-            boolean isPressed = button != 0;
-
-            if (isPressed) {
-                if (timePressed == 0) {
-                    timePressed = runtime.time();
-                }
-                timePressed = runtime.time() - timePressed;
-            }
-            else {
-                timePressed = 0.0;
-            }
-
-            pressTimes.put(key, new Double[] {button, timePressed});
+    void slowControl() {
+        boolean slow = gamepad1.left_bumper;
+        if (slow) {
+            frontLeftPower *= slowerFactor;
+            frontRightPower *= slowerFactor;
+            backLeftPower *= slowerFactor;
+            backRightPower *= slowerFactor;
         }
-
-        rightStickXPressTime = pressTimes.get("rightStickXPressTime")[1];
-        leftStickYPressTime = pressTimes.get("leftStickYPressTime")[1];
-        leftTriggerPressTime = pressTimes.get("leftTriggerPressTime")[1];
-        rightTriggerPressTime = pressTimes.get("rightTriggerPressTime")[1];
     }
 
-
-    void boostControl() {
+   void boostControl() {
         boolean boost = gamepad1.right_bumper;
         if (boost) {
-            frontLeftPower *= secondsToFullSpeed;
-            frontRightPower *= secondsToFullSpeed;
-            backLeftPower *= secondsToFullSpeed;
-            backRightPower *= secondsToFullSpeed;
+            frontLeftPower *= boostFactor;
+            frontRightPower *= boostFactor;
+            backLeftPower *= boostFactor;
+            backRightPower *= boostFactor;
         }
     }
 
     void pivotControl() {
-        double pivot = -gamepad1.right_stick_x * rightStickXPressTime / secondsToFullSpeed;
+        double pivot = gamepad1.right_stick_x * factor;
 
         controls.add(new Double[][] {
-                {pivot, pivot},
-                {pivot, pivot}
+                {pivot, -pivot},
+                {pivot, -pivot}
         });
     }
 
     void verticalControl() {
-        double y = gamepad1.left_stick_y * leftStickYPressTime / secondsToFullSpeed;
+        double y = -gamepad1.left_stick_y * factor;
 
         controls.add(new Double[][] {
-                {y, -y},
-                {y, -y}
+                {y, y},
+                {y, y}
         });
     }
 
     void horizontalSlideControl() {
-        double leftTrigger = gamepad1.left_trigger * leftTriggerPressTime / secondsToFullSpeed;
-        double rightTrigger = gamepad1.right_trigger * rightTriggerPressTime / secondsToFullSpeed;
+        double leftTrigger = gamepad1.left_trigger * factor;
+        double rightTrigger = gamepad1.right_trigger * factor;
 
         double slide = leftTrigger - rightTrigger;
 
@@ -150,5 +124,11 @@ public class Movement extends LinearOpMode {
                 {-slide, +slide},
                 {+slide, -slide}
         });
+    }
+
+    void intakeControl() {
+        boolean a = gamepad1.a;
+
+        intakeMotor.setPower(a ? 1 : 0);
     }
 }
